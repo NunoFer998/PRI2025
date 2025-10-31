b4004aeecd0354c417a9c3f01bcf83c90c8b335f
Mariana Pereira <marianappereira13@gmail.com>
2025-10-31 19:20:39 +0000

    Corrected schema and script

--- milestone2/startup.sh
+++ milestone2/startup.sh
@@ -6,11 +6,20 @@
 # Linux:
 # docker run -p 8983:8983 --name meic_solr -v "${PWD}/data:/data" -d solr:9 solr-precreate diseases
 
+sleep 10
 
 # Schema definition via API
+echo "Updating schema..."
+
 curl -X POST -H 'Content-type:application/json' \
     --data-binary "@data/schema.json" \
     http://localhost:8983/solr/diseases/schema
 
+echo "Allowing schema to reload..."
+sleep 5
+
 # Populate collection using mapped path inside container.
-docker exec -it meic_solr bin/solr post -c diseases /data/merged_disease_symptom_list.csv
+echo "Deleting old data and re-indexing..."
+docker exec -it meic_solr bin/solr post -c diseases /data/merged_disease_symptom_list.csv -params "overwrite=true"
+
+echo "Setup complete"
