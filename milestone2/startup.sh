7d9812c1cc51c279cf1129402c8e16b42d633304
Mariana Pereira <marianappereira13@gmail.com>
2025-11-14 12:17:49 +0000

    Automatically check if container is created

--- milestone2/startup.sh
+++ milestone2/startup.sh
@@ -6,20 +6,53 @@
 # Linux:
 # docker run -p 8983:8983 --name meic_solr -v "${PWD}/data:/data" -d solr:9 solr-precreate diseases
 
+CONTAINER_NAME="meic_solr"
+IMAGE="solr:9"
+HOST_PORT=8983
+LOCAL_DATA_PATH="${PWD}/data"
+SOLR_CORE="diseases"
+
+echo "Checking container status..."
+
+if ! docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo -e "Container '${CONTAINER_NAME}' does not exist. Creating it..."
+
+    docker run -p ${HOST_PORT}:8983 \
+        --name ${CONTAINER_NAME} \
+        -v "${LOCAL_DATA_PATH}:/data" \
+        -d ${IMAGE} solr-precreate ${SOLR_CORE}
+
+else
+    if ! docker ps --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+        echo -e "Container '${CONTAINER_NAME}' exists but is stopped. Starting it...\n"
+        docker start ${CONTAINER_NAME}
+    else
+        echo -e "Container '${CONTAINER_NAME}' is already running.\n"
+    fi
+fi
+
+echo -e "\nWaiting for Solr to start...\n"
 sleep 10
 
 # Schema definition via API
-echo "Updating schema..."
+echo "Checking if schema was already applied..."
 
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary "@data/schema.json" \
-    http://localhost:8983/solr/diseases/schema
+FIELD_EXISTS=$(curl -s "http://localhost:8983/solr/diseases/schema/field/name" | grep -c '"name"')
 
-echo "Allowing schema to reload..."
-sleep 5
+if [ "$FIELD_EXISTS" -gt 0 ]; then
+    echo -e "Schema already applied. Skipping schema update.\n"
+else
+    echo "Updating schema..."
+    curl -X POST -H 'Content-type:application/json' \
+        --data-binary "@data/schema.json" \
+        http://localhost:8983/solr/diseases/schema
+
+    echo -e "\nAllowing schema to reload...\n"
+    sleep 5
+fi
 
 # Populate collection using mapped path inside container.
-echo "Deleting old data and re-indexing..."
-docker exec -it meic_solr bin/solr post -c diseases /data/merged_disease_symptom_list.csv -params "overwrite=true"
+echo -e "Deleting old data and re-indexing...\n"
+docker exec -it ${CONTAINER_NAME} bin/solr post -c ${SOLR_CORE} /data/merged_disease_symptom_list.csv -params "overwrite=true"
 
-echo "Setup complete"
+echo -e "\nSetup complete!"
