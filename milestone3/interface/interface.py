8ce449713636ad3971b843a5b45138a36b958492
mxriana6 <marianapp64@gmail.com>
2025-12-17 16:55:55 +0000

    Missing import

--- milestone3/interface/interface.py
+++ milestone3/interface/interface.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import requests
 
 # Add parent directory to path for imports
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
